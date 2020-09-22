cab5f1fc00b9531379e1dee629dc316791a3a7b5
fenris <anatolijv236@gmail.com>
2020-09-22 21:31:36 +0300

    add slick

--- js/scripts.js
+++ js/scripts.js
@@ -42,6 +42,28 @@ $(document).ready(() => {
     });
 
     /**
+     * SLICK 22.09.2020
+     */
+    $('.promotions__items').slick({
+        infinite: true,
+        arrows: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev"><svg style="display: block" viewBox="0 0 10.6 18" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <desc>Left</desc> <polyline fill="none" stroke="#ffffff" stroke-linejoin="butt" stroke-linecap="butt" stroke-width="2" points="1,1 9,9 1,17"></polyline> </svg></button>',
+        nextArrow: '<button type="button" class="slick-next"><svg style="display: block" viewBox="0 0 10.6 18" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <desc>Right</desc> <polyline fill="none" stroke="#ffffff" stroke-linejoin="butt" stroke-linecap="butt" stroke-width="2" points="1,1 9,9 1,17"></polyline> </svg></button>',
+        slidesToShow: 2,
+        autoplay: false,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    /**
      * ANCHOR
      */
     $('.anchorLink').on('click', (e) => {
